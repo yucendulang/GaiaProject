d0bb6d06198b1c808a240f78d6b6ae243f8d6efc
325153468@qq.com <325153468@qq.com>
2017-11-10 14:16:54 +0800

    注册登录中文

--- GaiaProject/Models/AccountViewModels/LoginViewModel.cs
+++ GaiaProject/Models/AccountViewModels/LoginViewModel.cs
@@ -10,13 +10,15 @@ namespace GaiaProject.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "邮箱")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "记住我?")]
         public bool RememberMe { get; set; }
     }
 }
--- GaiaProject/Models/AccountViewModels/RegisterViewModel.cs
+++ GaiaProject/Models/AccountViewModels/RegisterViewModel.cs
@@ -10,17 +10,17 @@ namespace GaiaProject.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "邮箱")]
         public string Email { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "密码")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "确认密码")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
