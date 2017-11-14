cc2d0906f113b420fb9aa8079b5662637fa09ef9
Dulang <yucenyucen@126.com>
2017-11-15 02:09:05 +0800

    Fix

---
+++ GaiaCore/Gaia/Faction/FactionBackup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GaiaCore.Gaia
+{
+    public class FactionBackup
+    {
+        public int m_credit;
+        public int m_ore;
+        public int m_knowledge;
+        public int m_QICs;
+        public int m_powerToken1;
+        public int m_powerToken2;
+        public int m_powerToken3;
+        public int m_powerTokenGaia;
+    }
+}
