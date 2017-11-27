caf2d3d76d511bc91704d59683b598417d0c4b2f
325153468@qq.com <325153468@qq.com>
2017-11-27 17:24:35 +0800

    发送邮件插件

--- GaiaProject/Services/MessageServices.cs
+++ GaiaProject/Services/MessageServices.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace GaiaProject.Services
 {
@@ -14,6 +18,59 @@ namespace GaiaProject.Services
         {
             // Plug in your email service here to send an email.
             return Task.FromResult(0);
+        }
+
+        public Task Execute(string apiKey, string subject, string message, string email)
+        {
+            return null;
+//            var client = new SendGridClient(apiKey);
+//            var msg = new SendGridMessage()
+//            {
+//                From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+//                Subject = subject,
+//                PlainTextContent = message,
+//                HtmlContent = message
+//            };
+//            msg.AddTo(new EmailAddress(email));
+//            return client.SendEmailAsync(msg);
+        }
+
+
+        /// <summary>
+        /// 发送邮件
+        /// </summary>
+        /// <param name="mailTo">要发送的邮箱</param>
+        /// <param name="mailSubject">邮箱主题</param>
+        /// <param name="mailContent">邮箱内容</param>
+        /// <returns>返回发送邮箱的结果</returns>
+        public static bool SendEmail(string mailTo, string mailSubject, string mailContent)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Joey Tribbiani", "joey@friends.com"));
+            message.To.Add(new MailboxAddress("Mrs. Chanandler Bong", "chandler@friends.com"));
+
+            message.Subject = "星期天去哪里玩？";
+
+            message.Body = new TextPart("plain") { Text = "我想去故宫玩，如何" };
+
+            using (var client = new SmtpClient())
+            {
+                // For demo-purposes, accept all SSL certificates (in case the server supports STARTTLS)
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.friends.com", 587, false);
+
+                // Note: since we don't have an OAuth2 token, disable
+                // the XOAUTH2 authentication mechanism.
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                // Note: only needed if the SMTP server requires authentication
+                client.Authenticate("joey", "password");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
+            return true;
         }
 
         public Task SendSmsAsync(string number, string message)
