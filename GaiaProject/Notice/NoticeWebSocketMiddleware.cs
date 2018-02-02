f956a7d4ec2580ebac87660c69bfd99e3d3b97c5
325153468@qq.com <325153468@qq.com>
2018-02-02 15:06:32 +0800

    目前的测试代码

--- GaiaProject/Notice/NoticeWebSocketMiddleware.cs
+++ GaiaProject/Notice/NoticeWebSocketMiddleware.cs
@@ -90,26 +90,22 @@ namespace GaiaProject.Notice
                     break;
                 }
 
-                var response = await ReceiveStringAsync(currentSocket, ct);
-                if (string.IsNullOrEmpty(response))
+                if (currentSocket.State != WebSocketState.Open)
                 {
-                    if (currentSocket.State != WebSocketState.Open)
-                    {
-                        break;
-                    }
-
-                    continue;
+                    break;
                 }
 
-                foreach (var socket in socketList)
-                {
-                    if (socket.Value.State != WebSocketState.Open)
-                    {
-                        continue;
-                    }
+                continue;
 
-                    await SendStringAsync(socket.Value, response, ct);
-                }
+                //                foreach (var socket in socketList)
+                //                {
+                //                    if (socket.Value.State != WebSocketState.Open)
+                //                    {
+                //                        continue;
+                //                    }
+                //
+                //                    await SendStringAsync(socket.Value, response, ct);
+                //                }
             }
             //从socket删除
             WebSocket dummy;
@@ -126,58 +122,6 @@ namespace GaiaProject.Notice
         }
 
 
-        public async Task Invoke2(HttpContext context)
-        {
-            if (!context.WebSockets.IsWebSocketRequest)
-            {
-                await _next.Invoke(context);
-                return;
-            }
-
-            
-
-            CancellationToken ct = context.RequestAborted;
-            WebSocket currentSocket = await context.WebSockets.AcceptWebSocketAsync();
-            var socketId = Guid.NewGuid().ToString();
-
-           // _sockets.TryAdd(socketId, currentSocket);
-
-            while (true)
-            {
-                if (ct.IsCancellationRequested)
-                {
-                    break;
-                }
-
-                var response = await ReceiveStringAsync(currentSocket, ct);
-                if (string.IsNullOrEmpty(response))
-                {
-                    if (currentSocket.State != WebSocketState.Open)
-                    {
-                        break;
-                    }
-
-                    continue;
-                }
-
-//                foreach (var socket in _sockets)
-//                {
-//                    if (socket.Value.State != WebSocketState.Open)
-//                    {
-//                        continue;
-//                    }
-//
-//                    await SendStringAsync(socket.Value, response, ct);
-//                }
-            }
-
-            WebSocket dummy;
-            //_sockets.TryRemove(socketId, out dummy);
-
-            await currentSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", ct);
-            currentSocket.Dispose();
-        }
-
         private static Task SendStringAsync(WebSocket socket, string data, CancellationToken ct = default(CancellationToken))
         {
             var buffer = Encoding.UTF8.GetBytes(data);
@@ -185,37 +129,6 @@ namespace GaiaProject.Notice
             return socket.SendAsync(segment, WebSocketMessageType.Text, true, ct);
         }
 
-        private static async Task<string> ReceiveStringAsync(WebSocket socket, CancellationToken ct = default(CancellationToken))
-        {
-            var buffer = new ArraySegment<byte>(new byte[8192]);
-            using (var ms = new MemoryStream())
-            {
-                WebSocketReceiveResult result;
-                do
-                {
-                    ct.ThrowIfCancellationRequested();
-
-                    result = await socket.ReceiveAsync(buffer, ct);
-                    ms.Write(buffer.Array, buffer.Offset, result.Count);
-                }
-                while (!result.EndOfMessage);
-
-                ms.Seek(0, SeekOrigin.Begin);
-                if (result.MessageType != WebSocketMessageType.Text)
-                {
-                    return null;
-                }
-
-                // Encoding UTF8: https://tools.ietf.org/html/rfc6455#section-5.6
-//                using (var reader = new StreamReader(ms, Encoding.UTF8))
-//                {
-//                    return await reader.ReadToEndAsync();
-//                }
-                return "ss";
-
-            }
-
-        }
 
         /// <summary>
         /// 游戏行动提醒
@@ -229,7 +142,7 @@ namespace GaiaProject.Notice
             //包含游戏
             if (gameList.ContainsKey(gameName))
             {
-                ConcurrentDictionary<string,WebSocket> socketInfo = gameList[gameName];
+                ConcurrentDictionary<string, WebSocket> socketInfo = gameList[gameName];
                 //遍历用户
                 foreach (string socketInfoKey in socketInfo.Keys)
                 {
@@ -241,11 +154,20 @@ namespace GaiaProject.Notice
                         {
                             continue;
                         }
+                        socket.CloseAsync(WebSocketCloseStatus.Empty, "", cancellationToken: new CancellationToken());
+                        //socket = new WebSocket();
                         await SendStringAsync(socket, "200");
                     }
                 }
             }
-            
+            try
+            {
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             
         }
 
--- GaiaProject/Startup.cs
+++ GaiaProject/Startup.cs
@@ -66,7 +66,7 @@ namespace GaiaProject
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            services.AddMvc();
+            //services.AddMvc();
 
             // Add application services.
             services.AddTransient<IEmailSender, AuthMessageSender>();
