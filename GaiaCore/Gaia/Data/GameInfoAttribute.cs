d643c82de6c9f1d2cf7f2753fc4a4f575f2e6a9c
325153468@qq.com <325153468@qq.com>
2019-01-22 12:13:14 +0800

    完善英文界面

--- GaiaCore/Gaia/Data/GameInfoAttribute.cs
+++ GaiaCore/Gaia/Data/GameInfoAttribute.cs
@@ -6,6 +6,20 @@ namespace GaiaCore.Gaia.Data
 {
     public class GameInfoAttribute
     {
+        ///// <summary>
+        ///// 地图模式
+        ///// </summary>
+        //public static readonly List<PwInfo> MapSelction = new List<PwInfo>()
+        //{
+        //    new PwInfo(){code = "fix2p",name = "双人固定"},
+        //    new PwInfo(){code = "random2p",name = "双人随机"},
+        //    new PwInfo(){code = "fix3p",name = "双人变体固定"},
+        //    new PwInfo(){code = "random3p",name = "双人变体随机"},
+        //    new PwInfo(){code = "fix4p",name = "四人固定"},
+        //    new PwInfo(){code = "random4p",name = "四人部分随机"},
+        //    new PwInfo(){code = "randomall4p",name = "四人完全随机"},
+        //};
+
         /// <summary>
         /// 地图模式
         /// </summary>
@@ -13,13 +27,14 @@ namespace GaiaCore.Gaia.Data
         {
             new PwInfo(){code = "fix2p",name = "双人固定"},
             new PwInfo(){code = "random2p",name = "双人随机"},
-            new PwInfo(){code = "fix3p",name = "双人变体固定"},
-            new PwInfo(){code = "random3p",name = "双人变体随机"},
+            //new PwInfo(){code = "fix3p",name = "双人变体固定"},
+            //new PwInfo(){code = "random3p",name = "双人变体随机"},
             new PwInfo(){code = "fix4p",name = "四人固定"},
-            new PwInfo(){code = "random4p",name = "四人部分随机"},
+            //new PwInfo(){code = "random4p",name = "四人部分随机"},
             new PwInfo(){code = "randomall4p",name = "四人完全随机"},
 
         };
+
         /// <summary>
         /// 连续不行动的drop时间,暂时没有1,5,10,
         /// </summary>
